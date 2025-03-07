79dac8fb9b321851359ecbb50d9527e6f98c0043
DavidProm <davidpinedatorres@hotmail.es>
2025-03-07 13:02:04 -0500

    Incorporar rutas de navegacion

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -1,57 +1,73 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
-import { Product } from '../models/product';
-import { CatalogComponent } from './catalog/catalog.component';
 import { CartItem } from '../models/cartItems';
 import { NavbarComponent } from './navbar/navbar.component';
+import { Router, RouterOutlet } from '@angular/router';
+import { SharingDataService } from '../services/sharing-data.service';
 
 @Component({
   selector: 'cart-app',
-  imports: [CatalogComponent, NavbarComponent],
+  imports: [NavbarComponent, RouterOutlet],
   templateUrl: './cart-app.component.html'
 })
 export class CartAppComponent implements OnInit {
-
-  products: Product[] = [];
 
   items: CartItem[] = [];
 
   total: number = 0;
 
-  constructor(private service: ProductService) { }
+  constructor(
+    private router: Router,
+    private sharingDataService: SharingDataService, 
+    private service: ProductService) { }
 
   ngOnInit(): void {
-    this.products = this.service.findAll();
     this.items = JSON.parse(sessionStorage.getItem('cart')!) || [];
     this.calculateTotal();
+    this.onDeleteCart();
+    this.onAddcart();
   }
 
-  onAddcart(product: Product): void {
-    const hasItem = this.items.find(item => item.product.id === product.id) //=> define una función anónima más corta
-    if (hasItem) {
-      this.items = this.items.map(item => {
-        if (item.product.id === product.id) {
-          return {
-            ...item,
-            quantity: item.quantity + 1
+  onAddcart(): void {
+    this.sharingDataService.productEventEmitter.subscribe( product => {
+      const hasItem = this.items.find(item => item.product.id === product.id) //=> define una función anónima más corta
+      if (hasItem) {
+        this.items = this.items.map(item => {
+          if (item.product.id === product.id) {
+            return {
+              ...item,
+              quantity: item.quantity + 1
+            }
           }
-        }
-        return item;
+          return item;
+        })
+      } else {
+        this.items = [... this.items, { product: { ...product }, quantity: 1 }]
+      }
+      this.calculateTotal();
+      this.saveSession();
+      this.router.navigate(['/cart'],{
+        state: {items: this.items, total: this.total}
       })
-    } else {
-      this.items = [... this.items, { product: { ...product }, quantity: 1 }]
-    }
-    this.calculateTotal();
-    this.saveSession();
+    });
   }
 
-  onDeleteCart(id: number): void {
-    this.items = this.items.filter(item => item.product.id !== id); //filter es el metodo para eliminar o filtrar
-    if(this.items.length == 0){
-      sessionStorage.removeItem('cart');
-    }
-    this.calculateTotal();
-    this.saveSession();
+  onDeleteCart(): void {
+    this.sharingDataService.idProductEventEmitter.subscribe(id => {
+      console.log(id + 'se ha ejecutado el evento idProductEventEmitter')
+      this.items = this.items.filter(item => item.product.id !== id); //filter es el metodo para eliminar o filtrar
+      if (this.items.length == 0) {
+        sessionStorage.removeItem('cart');
+      }
+      this.calculateTotal();
+      this.saveSession();
+
+      this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+        this.router.navigate(['/cart'],{
+          state: {items: this.items, total: this.total}
+        })
+      })
+    });
   }
 
   calculateTotal(): void {
--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter} from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 import { CartItem } from '../../models/cartItems';
+import { Router } from '@angular/router';
+import { SharingDataService } from '../../services/sharing-data.service';
 
 @Component({
   selector: 'cart',
@@ -12,10 +14,13 @@ export class CartComponent {
 
   total = 0;
 
-  idProductEventEmitter = new EventEmitter();
+  constructor(private sharingDataService: SharingDataService, private router: Router) {
+    this.items = this.router.getCurrentNavigation()?.extras.state!['items'];
+    this.total = this.router.getCurrentNavigation()?.extras.state!['total'];
+  }
 
   onDeleteCart(id: number) {
-    this.idProductEventEmitter.emit(id);
+    this.sharingDataService.idProductEventEmitter.emit(id);
   }
 
 }
