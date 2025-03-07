7e684d3f66bc8b61a151d3193845bc8f7966da67
DavidProm <davidpinedatorres@hotmail.es>
2025-03-06 22:46:54 -0500

    Primer commit del proyecto Angular

---
+++ src/app/components/cart-app.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
+import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItem } from '../models/cartItems';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@Component({
+  selector: 'cart-app',
+  imports: [CatalogComponent, CartComponent, NavbarComponent],
+  templateUrl: './cart-app.component.html'
+})
+export class CartAppComponent implements OnInit {
+
+  products: Product[] = [];
+
+  items: CartItem[] = [];
+
+  total: number = 0;
+
+  showCart: boolean = false;
+
+  constructor(private service: ProductService) { }
+
+  ngOnInit(): void {
+    this.products = this.service.findAll();
+    this.items = JSON.parse(sessionStorage.getItem('cart')!) || [];
+    this.calculateTotal();
+  }
+
+  onAddcart(product: Product): void {
+    const hasItem = this.items.find(item => item.product.id === product.id) //=> define una función anónima más corta
+    if (hasItem) {
+      this.items = this.items.map(item => {
+        if (item.product.id === product.id) {
+          return {
+            ...item,
+            quantity: item.quantity + 1
+          }
+        }
+        return item;
+      })
+    } else {
+      this.items = [... this.items, { product: { ...product }, quantity: 1 }]
+    }
+    this.calculateTotal();
+    this.saveSession();
+  }
+
+  onDeleteCart(id: number): void {
+    this.items = this.items.filter(item => item.product.id !== id); //filter es el metodo para eliminar o filtrar
+    this.calculateTotal();
+    this.saveSession();
+  }
+
+  calculateTotal(): void {
+    this.total = this.items.reduce((accumalator, item) => accumalator + item.quantity * item.product.price, 0);
+  }
+
+  saveSession(): void {
+    sessionStorage.setItem('cart', JSON.stringify(this.items)); //JSON.stringity convierte un objeto a texto como
+  }
+
+  openCart(): void {
+    this.showCart = !this.showCart;
+  }
+}
---
+++ src/app/data/product.data.ts
@@ -0,0 +1,44 @@
+import { Product } from "../models/product";
+
+export const products: Product[] = [
+    {
+        id: 1,
+        name: 'Teclado macanico  Asus RGB',
+        description: 'Teclado con teclas de tipo cherry red',
+        price: 1000
+    },
+
+    {
+
+        id: 2,
+        name: 'Samsung Smart Tv',
+        description: 'Pantalla bella la verdad',
+        price: 200
+    },
+    {
+        id: 3,
+        name: 'Sonic video',
+        description: 'Buen renderizado de videos ',
+        price: 100
+    },
+
+    {
+        id: 4,
+        name: 'Corsair RAM 16',
+        description: 'Memoria ram para videojuegos',
+        price: 4000
+    },
+    {
+        id: 5,
+        name: 'Nvidia ASUS 4080 RTX',
+        description: 'Targeta de video Nvideo optimizada para tareas multicor',
+        price: 3000
+    },
+    {
+        id: 6,
+        name: 'CPU Ryzen 7',
+        description: 'Refigeracion interna incorporada',
+        price: 6000
+    },
+
+]
