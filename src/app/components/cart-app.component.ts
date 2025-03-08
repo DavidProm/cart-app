d4549e85631e53514f1ed7efa9115a71eabe8483
DavidProm <davidpinedatorres@hotmail.es>
2025-03-07 19:28:05 -0500

    Mensajes de Alertas

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -4,6 +4,7 @@ import { CartItem } from '../models/cartItems';
 import { NavbarComponent } from './navbar/navbar.component';
 import { Router, RouterOutlet } from '@angular/router';
 import { SharingDataService } from '../services/sharing-data.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'cart-app',
@@ -49,24 +50,46 @@ export class CartAppComponent implements OnInit {
       this.router.navigate(['/cart'],{
         state: {items: this.items, total: this.total}
       })
+      Swal.fire({
+        title: "Shopping",
+        text: "Nuevo producto agregado el carro!",
+        icon: "success"
+      });
     });
   }
 
   onDeleteCart(): void {
     this.sharingDataService.idProductEventEmitter.subscribe(id => {
       console.log(id + 'se ha ejecutado el evento idProductEventEmitter')
-      this.items = this.items.filter(item => item.product.id !== id); //filter es el metodo para eliminar o filtrar
-      if (this.items.length == 0) {
-        sessionStorage.removeItem('cart');
-      }
-      this.calculateTotal();
-      this.saveSession();
-
-      this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
-        this.router.navigate(['/cart'],{
-          state: {items: this.items, total: this.total}
-        })
-      })
+      Swal.fire({
+        title: "Estas seguro que deseas eliminar?",
+        text: "Cuidado el producto se eliminara del carro de compras!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, hazlo!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.items = this.items.filter(item => item.product.id !== id); //filter es el metodo para eliminar o filtrar
+          if (this.items.length == 0) {
+            sessionStorage.removeItem('cart');
+          }
+          this.calculateTotal();
+          this.saveSession();
+    
+          this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+            this.router.navigate(['/cart'],{
+              state: {items: this.items, total: this.total}
+            })
+          })
+          Swal.fire({
+            title: "Eliminado!",
+            text: "Se ha eliminado el producto",
+            icon: "success"
+          });
+        }
+      });
     });
   }
 
