d2ba0a75758702fc904976adac0aadba617f8dfd
DavidProm <davidpinedatorres@hotmail.es>
2025-03-08 13:06:47 -0500

    Optimizacion del Codigo con Redux

--- src/app/components/cart-app.component.ts
+++ src/app/components/cart-app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../services/product.service';
 import { CartItem } from '../models/cartItems';
 import { NavbarComponent } from './navbar/navbar.component';
 import { Router, RouterOutlet } from '@angular/router';
 import { SharingDataService } from '../services/sharing-data.service';
 import Swal from 'sweetalert2'
+import { Store } from '@ngrx/store';
+import { ItemsState } from '../store/items.reducer';
+import { add, remove, total } from '../store/items.actions';
 
 @Component({
   selector: 'cart-app',
@@ -18,38 +20,28 @@ export class CartAppComponent implements OnInit {
   total: number = 0;
 
   constructor(
+    private store: Store<{ items: ItemsState }>,
     private router: Router,
-    private sharingDataService: SharingDataService, 
-    private service: ProductService) { }
+    private sharingDataService: SharingDataService,) {
+    this.store.select('items').subscribe(state => {
+      this.items = state.items;
+      this.saveSession();
+    });
+  }
 
   ngOnInit(): void {
-    this.items = JSON.parse(sessionStorage.getItem('cart')!) || [];
-    this.calculateTotal();
     this.onDeleteCart();
     this.onAddcart();
   }
 
   onAddcart(): void {
-    this.sharingDataService.productEventEmitter.subscribe( product => {
-      const hasItem = this.items.find(item => item.product.id === product.id) //=> define una función anónima más corta
-      if (hasItem) {
-        this.items = this.items.map(item => {
-          if (item.product.id === product.id) {
-            return {
-              ...item,
-              quantity: item.quantity + 1
-            }
-          }
-          return item;
-        })
-      } else {
-        this.items = [... this.items, { product: { ...product }, quantity: 1 }]
-      }
-      this.calculateTotal();
-      this.saveSession();
-      this.router.navigate(['/cart'],{
-        state: {items: this.items, total: this.total}
-      })
+    this.sharingDataService.productEventEmitter.subscribe(product => {
+
+      this.store.dispatch(add({ product }));
+      this.store.dispatch(total());
+
+      this.router.navigate(['/cart']);
+
       Swal.fire({
         title: "Shopping",
         text: "Nuevo producto agregado el carro!",
@@ -71,18 +63,12 @@ export class CartAppComponent implements OnInit {
         confirmButtonText: "Si, hazlo!"
       }).then((result) => {
         if (result.isConfirmed) {
-          this.items = this.items.filter(item => item.product.id !== id); //filter es el metodo para eliminar o filtrar
-          if (this.items.length == 0) {
-            sessionStorage.removeItem('cart');
-          }
-          this.calculateTotal();
-          this.saveSession();
-    
-          this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
-            this.router.navigate(['/cart'],{
-              state: {items: this.items, total: this.total}
-            })
-          })
+
+          this.store.dispatch(remove({ id }));
+          this.store.dispatch(total());
+
+          this.router.navigate(['/cart']);
+          
           Swal.fire({
             title: "Eliminado!",
             text: "Se ha eliminado el producto",
@@ -91,10 +77,6 @@ export class CartAppComponent implements OnInit {
         }
       });
     });
-  }
-
-  calculateTotal(): void {
-    this.total = this.items.reduce((accumalator, item) => accumalator + item.quantity * item.product.price, 0);
   }
 
   saveSession(): void {
